2e86c0593798d29b4284bcffc89b6f053ca37ba2
jaybee <mandolle777@gmail.com>
2026-03-03 14:36:32 +0900

    Update runDockerContainer.groovy

--- vars/runDockerContainer.groovy
+++ vars/runDockerContainer.groovy
@@ -1,6 +1,23 @@
 def call(Map cfg) {
+
+    def memory = cfg.memory ?: "1g"
+    def springProfile = cfg.profile ?: "prod"
+
+    def dockerOpts = []
+    dockerOpts << "-d"
+    dockerOpts << "--restart unless-stopped"
+    dockerOpts << "--memory ${memory}"
+    dockerOpts << "--name ${cfg.newDocker}"
+    dockerOpts << "-p ${cfg.newPort}:${cfg.appPort}"
+
+    if(cfg.network) {
+        dockerOpts << "--network ${cfg.network}"
+    }
+
+    dockerOpts << "-e SPRING_PROFILES_ACTIVE=${springProfile}"
+
     sh """
         docker rm -f ${cfg.newDocker} || true
-        docker run -d --name ${cfg.newDocker} -p ${cfg.newPort}:${cfg.appPort} ${cfg.imageName}:${cfg.tag}
+        docker run ${dockerOpts.join(' ')} ${cfg.imageName}:${cfg.tag}
     """
 }
